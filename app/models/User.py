d83c3e8e8095f6c2d03eac88858fc746510ea5d3
Benjamin <benjamind10@pm.me>
2022-06-19 22:39:11 -0400

    finished models and db

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,10 @@
+import bcrypt
+
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+
+salt = bcrypt.gensalt()
 
 class User(Base):
   __tablename__ = 'users'
@@ -7,3 +12,16 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+  @validates('email')
+  def validate_email(self, key, email):
+    # make sure email address contains @ character
+    assert '@' in email
+
+    return email
